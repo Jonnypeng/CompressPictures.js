f9be2c5f8497dfd45a1e7909c7d6efba9e28ad30
jonny <jonny.peng@qq.com>
2018-06-12 19:27:07 +0800

    导入exif.js,然后完成可以旋转的版本

--- README.md
+++ README.md
@@ -1,42 +1,46 @@
 <div class="col-xs-12 col-md-12">
+  <h4>起步：</h4>
+  <p>
+    先导入外部的<a href="https://github.com/exif-js/exif-js">exif.js<a>,这是读取图片exif数据的库，在这里我们主要获取照片的方向,如果方向为1，为正；如果方向为6，则需要旋转;
+    然后导入build的compressPictures.js;
+  </p>
   <h4>构造：</h4>
   <p>
     new compressPictures(imgData,ratio,encoder);
   </p>
-<table class="table table-bordered">
-<thead>
-<tr>
-<th>Name</th>
-<th>Type</th>
-<th>Description</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>imgData</td>
-<td>HTML Img Element	</td>
-<td>需要压缩的DOM图片元素</td>
-</tr>
-<tr>
-<td>ratio</td>
-<td>Number</td>
-<td>默认为0，最大值为1</td>
-</tr>
-<tr>
-<td>encoder</td>
-<td>String</td>
-<td>默认值为"images/png",可以为"image/jpeg"</td>
-</tr>
-</tbody>
-</table>
-<h4>压缩方法：</h4>
-<p>
-compress();
-</p>
-<h4>举例:</h4>
-<p>
-var data = new compressPictures(tempImg,0.3,"image/jpeg");<br>
-img.setAttribute("src",data.compress());
-</p>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>imgData</td>
+        <td>HTML Img Element	</td>
+        <td>需要压缩的DOM图片元素</td>
+      </tr>
+      <tr>
+        <td>ratio</td>
+        <td>Number</td>
+        <td>默认为0，最大值为1</td>
+      </tr>
+      <tr>
+        <td>encoder</td>
+        <td>String</td>
+        <td>默认值为"images/png",可以为"image/jpeg"</td>
+      </tr>
+    </tbody>
+  </table>
+  <h4>属性</h4>
+  <p>
+    compress; #这就是一个已经压缩的base64编码；
+  </p>
+  <h4>举例:</h4>
+  <p>
+    var data = new compressPictures(tempImg,0.3,"image/jpeg");<br>
+    img.setAttribute("src",data.compress);
+  </p>
 </div>
-This library is available under the MIT license.
--- build/compressPictures.js
+++ build/compressPictures.js
@@ -10,27 +10,63 @@ class compressPictures{
 		this.canvas = document.createElement("canvas");
 		this.ctx = this.canvas.getContext("2d");
 		this.DataURL;
-		this.init();
+		this.orientation = this.init();
 	}
 	init(){
+		var img = this.imgData;
+		var orient;
+		EXIF.getData(img, function () {
+          orient = EXIF.getTag(this, 'Orientation');
+    });
 		this.canvas.setAttribute("width",this.size.compress.width);
 		this.canvas.setAttribute("height",this.size.compress.height);
 		this.canvas.style.display = "none";
 		document.body.append(this.canvas);
+		return orient;
 	}
-	compress(){
-		this.ctx.drawImage(
-			this.imgData,
-			0,
-			0,
-			this.size.natural.width,
-			this.size.natural.height,
-			0,
-			0,
-			this.size.compress.width,
-			this.size.compress.height);
-		this.DataURL = this.canvas.toDataURL(this.encoder);
-		document.body.removeChild(this.canvas);
-		return this.DataURL;
+	get compress(){
+		this.orientation = 6;
+		if(this.orientation==1 || undefined){
+		console.log(this.orientation);
+			this.ctx.drawImage(
+				this.imgData,
+				0,
+				0,
+				this.size.natural.width,
+				this.size.natural.height,
+				0,
+				0,
+				this.size.compress.width,
+				this.size.compress.height);
+			this.DataURL = this.canvas.toDataURL(this.encoder);
+			document.body.removeChild(this.canvas);
+			return this.DataURL;
+		}else if(this.orientation==6){
+			this.canvas.setAttribute("height",this.size.compress.width);
+			this.canvas.setAttribute("width",this.size.compress.height);
+			console.log(this.orientation);
+			this.ctx.setTransform(1,0,0,1,0,0);
+			this.ctx.rotate((Math.PI/180)*90);
+			this.ctx.translate(0,this.size.compress.height*-1);
+			this.ctx.drawImage(
+				this.imgData,
+				0,
+				0,
+				this.size.natural.width,
+				this.size.natural.height,
+				0,
+				0,
+				this.size.compress.width,
+				this.size.compress.height);
+			this.DataURL = this.canvas.toDataURL(this.encoder);
+			document.body.removeChild(this.canvas);
+			return this.DataURL;
+		}else{
+			return false;
+		};
 	}
-}
+};
+
+function compress(){
+		console.log(this);
+};
